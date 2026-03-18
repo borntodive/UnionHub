682e224856b5e2605007ebb2f75c5f236d7f0e59
Andrea Covelli <andrea.covelli@gmail.com>
2026-03-18 10:42:16 +0100

    fix: correct migration class name syntax
    - Fix invalid class name with shell syntax
    - Use proper TypeScript class name

--- api/src/database/migrations/1773825770-DisableMustChangePassword.ts
+++ api/src/database/migrations/1773825770-DisableMustChangePassword.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class DisableMustChangePassword$(date +%s) implements MigrationInterface {
-  name = 'DisableMustChangePassword$(date +%s)';
+export class DisableMustChangePassword1773825770 implements MigrationInterface {
+  name = 'DisableMustChangePassword1773825770';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
