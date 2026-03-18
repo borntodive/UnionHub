896cd1ba1682f4c3599c1280564d95d9aa601eb6
Andrea Covelli <andrea.covelli@gmail.com>
2026-03-18 10:44:06 +0100

    fix: rename migration file to match TypeORM convention
    - Rename file from timestamp-prefix to timestamp-suffix
    - Remove redundant name property from class

--- api/src/database/migrations/DisableMustChangePassword1773825770.ts
+++ api/src/database/migrations/DisableMustChangePassword1773825770.ts
@@ -1,7 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class DisableMustChangePassword1773825770 implements MigrationInterface {
-  name = 'DisableMustChangePassword1773825770';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
